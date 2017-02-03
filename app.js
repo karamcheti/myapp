b163abcc20382089533cb11f8cc84c1c3cef7c51
karamcheti <rajendra.karamcheti@gmail.com>
2017-02-03 15:07:07 -0500

    Initial app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+var http      = require('http');
+var express   = require('express');
+app = express();
+
+app.get('/', function(req, res){
+  res.send("Hello World!");
+})
+
+app.listen(8080, function () {
+  console.log('It\'s showtime on port 8080 baby!');
+});
